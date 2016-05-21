b4644bcf030fb5f2941213db3bd89726edcfd652
Nisse Bergman <nisse@potmo.com>
2016-05-21 22:43:59 +0200

    create folders for spool before starting

--- run-sms.sh
+++ run-sms.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
+
+mkdir -p /home/pi/screamer/sent
+mkdir -p /home/pi/screamer/outgoing
+mkdir -p /home/pi/screamer/incoming
+mkdir -p /home/pi/screamer/checked
+mkdir -p /home/pi/screamer/failed
+mkdir -p /home/pi/screamer/stats
+
 /usr/sbin/smsd -t -nMAINPROCESS -c/home/pi/screamer/smsd.conf
